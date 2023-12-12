571ed39aa09749113a008c4ec602e4e958f9ca94
Lab IF <if@it.fti.unand.ac.id>
2023-12-12 11:42:00 +0700

    first commit

---
+++ SupermarketApp.java
@@ -0,0 +1,17 @@
+public class SupermarketApp {
+    public static void main(String[] args) {
+
+        Customer customer = new Customer("", "", "SuperMart", "");
+        customer.displaySupermarketInfo();
+        System.out.println("=====DATA PELANGGAN=====");
+        System.out.println(" ");
+
+        customer.inputCustomerData();
+
+        Purchase purchase = new Purchase("SuperMart", "John");
+        System.out.println("--DATA PEMBELIAN BARANG-");
+        purchase.inputPurchaseData();
+
+        purchase.displayPurchaseData();
+    }
+}
